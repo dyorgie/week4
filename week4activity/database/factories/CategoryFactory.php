ac248fd508200f7e3c5f4685784f6f016a3c478d
dyorgie <tejjlagmay@gmail.com>
2025-06-22 22:53:07 +0800

    initial factories edit

--- week4activity/database/factories/CategoryFactory.php
+++ week4activity/database/factories/CategoryFactory.php
@@ -3,12 +3,14 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
  */
 class CategoryFactory extends Factory
 {
+
     /**
      * Define the model's default state.
      *
@@ -16,8 +18,11 @@ class CategoryFactory extends Factory
      */
     public function definition(): array
     {
+        $catName = fake()->words(rand(1, 3), true);
         return [
-            //
+            'category_name' => $catName,
+            'slug' => Str::slug($catName),
+            'description' => fake()->sentence(rand(1, 5))
         ];
     }
 }
--- week4activity/database/factories/PostFactory.php
+++ week4activity/database/factories/PostFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -16,8 +17,15 @@ class PostFactory extends Factory
      */
     public function definition(): array
     {
+        $title = fake()->sentence(5);
+        $status = fake()->randomElement(['D', 'P', 'I']);
         return [
-            //
+            'title' => $title,
+            'content' => fake()->paragraph(),
+            'slug' => Str::slug($title),
+            'publication_date' => $status == 'Published' ? now() : null,
+            'status' => $status,
+            'featured_image_url' => fake()->imageUrl(640, 480, 'animals', true)
         ];
     }
 }
